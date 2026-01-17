cb52393e490bfcb9307d9571bd4b21ea42ccb845
Nikhil sharma <nikhillsharma455@gmail.com>
2026-01-17 20:52:19 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php
+  $CollegeName = "K.R. Mangalam University <br>";
+  $Regno = "2021KRMU1234 <br>";
+  $Branchname = "Computer Science <br>";
+
+  define("PHONENO", "9876543210 <br>");
+  define("DEPARTMENT", "Engineering");
+
+echo"variables in PHP <br>";
+  echo $CollegeName;
+  echo $Regno ;
+  echo $Branchname ;
+
+  echo "<br> Constants in PHP <br>";
+  echo PHONENO ;
+  echo DEPARTMENT;
+?>
